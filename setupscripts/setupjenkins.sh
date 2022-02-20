fb08e33a2357faabff32c70c808d2f6e63b20bc3
gowriand <95476026+gowriand@users.noreply.github.com>
2022-02-20 20:42:46 +0530

    Update setupjenkins.sh

--- setupscripts/setupjenkins.sh
+++ setupscripts/setupjenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-java_ver="openjdk-8-jdk"
+#java_ver="openjdk-8-jdk"
+java_ver="openjdk-11-jdk"
 echo "Setup apt-key"
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
