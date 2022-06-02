228e6b192ea59fb787f801cf0f97d44e76801271
gowriand <95476026+gowriand@users.noreply.github.com>
2022-06-02 19:13:20 +0530

    Update setup-jenkins.sh

--- setupscripts/setup-jenkins.sh
+++ setupscripts/setup-jenkins.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-java_ver="openjdk-8-jdk"
-#java_ver="openjdk-11-jdk"
-echo "Setup apt-key"
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
+#java_ver="openjdk-8-jdk"
+java_ver="openjdk-11-jdk"
 
 echo "Install $java_ver"
+sudo apt update
 sudo apt-get install -y $java_ver
 echo "INSTALLED JAVA................." 
 java -version
 
-echo "Install jenkins"
+
+echo "Setup apt-key"
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+
+echo "Install jenkins................................."
 sudo apt install -y jenkins
 sleep 30
 ps aux |grep jenkins
@@ -24,7 +31,5 @@ runuser -l  jenkins  -c 'cp /jenkins/setupscripts/ssh/id_rsa  /var/lib/jenkins/.
 chmod 600 /var/lib/jenkins/.ssh/id_rsa
  
 echo "INSTALLED  JENKINS................." 
-#sudo cp /var/lib/jenkins/secrets/initialAdminPassword /var/lib/jenkins/secrets/initialAdminPassword_bak
-#sudo cp initialAdminPassword /var/lib/jenkins/secrets/initialAdminPassword
 echo "USE PASS to SETUP: "`cat /var/lib/jenkins/secrets/initialAdminPassword`
 
